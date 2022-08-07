f6b99e3273cbf5b3aec7dce91579ade818d5c297
ilterkaynak <110779575+ilterkaynak@users.noreply.github.com>
2022-08-07 19:42:22 +0300

    task
    odev

---
+++ PHP/bootsrapSitemiz/dosya.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+	 
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    
+    <link rel="stylesheet" href="css/style.css"
+    
+    <title>Araç Kiralama</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/722f2f2f22.js" crossorigin="anonymous"></script>
+  </head>
+  <body>
+  
+  <!--Menü Kısmı Başlangıcı-->
+  
+  <nav class="navbar navbar-expand-lg bg-light fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">VIZ GELİR-VIZ GİDER</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#menu">Anasayfa</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#hakkimizda">Hakkımızda</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#filomuz">Filomuz</a>
+        </li>
+       
+       <li class="nav-item">
+          <a class="nav-link" href="#marka modellerimiz">Marka Modellerimiz</a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link" href="#iletisim">İletişim</a>
+        </li>
+       
+       
+       
+       
+       
+       
+       
+       
+       
+      </ul>
+    </div>
+  </div>
+</nav>
+  
+  
+   
+      
+    <!--Menü Kısmı Bitiş-->  
+      
+      
+   <!--Hakkımızda Başlangıç--> 
+     
+     <section class="p-5 text-center hakkimizda">
+         
+         <div class="container">
+             
+             <h2 class="mb-5 font-wight-bold">Hakkımızda</h2>
+             
+            <hr class="ayrac"><i class="fa-solid fa-leaf igri"></i>
+            
+             
+             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem quasi repellendus fugit, pariatur dolorum, facere consequuntur iure ipsum fuga cumque tempora quisquam consectetur, porro minima nesciunt fugiat blanditiis. Voluptatibus, veniam.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit fuga impedit iusto, praesentium nisi obcaecati dolorum. Consectetur nisi quaerat totam nostrum reprehenderit, assumenda itaque! Officiis praesentium neque nobis, explicabo blanditiis.</p>
+             
+             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ipsam assumenda amet, quibusdam dignissimos! Adipisci libero, officia nulla qui itaque tenetur saepe atque veritatis voluptatibus nemo iste sed. Consectetur, laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo soluta dignissimos voluptatum ipsa accusamus, corporis nesciunt qui dolorum. Illum reprehenderit laudantium aliquam eaque. Ab quam atque enim, dolore a autem.</p>
+             
+         </div>
+     </section>
+     
+     
+   <!--Hakkımızda Bitiş-->   
+      
+      
+           <!--Filomuz Başlangıç-->  
+           
+           <section class="p-5 text-center filomuz">
+              
+  <div class="container">
+            
+            <h2 class="mb-5 font-wight-bold">Filomuz</h2>
+             
+            <hr class="ayrac"><i class="fa-solid fa-leaf ibeyaz"></i>
+            
+            
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="row">
+    <div class="card shadow">
+      <img src="img/benyamin-bohlouli-Hg8oqApmjMw-unsplash.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Mini Cooper</h5>
+        <p class="card-text">2020 Model</p>
+      </div>
+      <div class="card-footer">
+       
+      </div>
+    </div>
+    
+
+  
+  
+</div>
+      </div>
+             
+               </div>
+              
+              
+               </section>
+           
+           
+           
+           
+  <!--Filomuz Bitiş-->  
+   
+      
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
